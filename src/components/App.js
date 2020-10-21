1cdf60c0bce5df2054f3ce2bc0f82b13010e1590
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-10-21 21:12:04 +0530

    Changes1

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,31 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import { parseAsync } from "@babel/core";
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+  }
+  // [paragraph, setParagraph] = React.useState("");
+  this.state = [paragraph];
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  this.renderPara = function () {
+    return (React.state.paragraph =
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  };
+
+  render() {
+    return (
+      <div id="main">
+        <button onClick={renderPara()} id={"click"}>
+          {" "}
+          Render Paragraph{" "}
+        </button>
+        <p id={"para"}> {React.state.paragraph} </p>
+        {/* Do not remove this main div!! */}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
